d5f7bb95dc8dc1a66e45e9274c00c9c44a4b55af
FranRitucci <fritucci1037@gmail.com>
2024-01-22 17:25:10 -0300

    otro intento

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function agregarContacto() {
     // Datos del nuevo contacto
     var nuevoNombre = "Sebastian Fontana";
-    var nuevoNumero = "+1 786-618-4924";
+    var nuevoNumero = "1 7866184924";
     var nuevoEmail = "sebastianfontana@yourithero.com";
 
     // Crear el enlace 'tel:' con el número de teléfono del nuevo contacto
     var enlaceTelefono = "tel:" + nuevoNumero + ";";
 
     // Abrir la libreta de contactos del teléfono con los datos predefinidos
-    window.location.href = enlaceTelefono + "contactName=" + encodeURIComponent(nuevoNombre) +
+    window.location.href = enlaceTelefono + "name=" + encodeURIComponent(nuevoNombre) +
         "&email=" + encodeURIComponent(nuevoEmail);
 }
